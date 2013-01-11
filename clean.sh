3d728956687744e8dc01c6c30e1706b00d15d6b3
kphongph <kphongph@gmail.com>
2013-01-11 14:54:24 +0700

    updat template

---
+++ clean.sh
@@ -0,0 +1,1 @@
+curl -v -H "Content-Type: application/json" -X POST -d '{"name":"nodejs-mongogui","user":"kphongph"}' http://10.10.20.75:9002/drones/nodejs-mongogui/clean
---
+++ stop.sh
@@ -0,0 +1,1 @@
+curl -v -H "Content-Type: application/json" -X POST -d '{"stop":{"name":"nodejs-mongogui"}}' http://10.10.20.75:9002/drones/nodejs-mongogui/stop
